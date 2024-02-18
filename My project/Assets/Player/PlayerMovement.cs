bc714d3be8011e41bb636203ff0c89afeaaf56f2
dnzgo <dnzgzc.98@gmail.com>
2024-02-18 21:52:23 +0300

    Added Player Movement

---
+++ My project/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Joystick moveJoystick;
+    [SerializeField] float moveSpeed = 5.0f;
+
+    Vector2 moveInput;
+
+    private void Start()
+    {
+
+        moveJoystick.onStickValueUpdated += MoveInputUpdated;
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveInput * moveSpeed * Time.deltaTime);
+    }
+
+    private void MoveInputUpdated(Vector2 inputValue)
+    {
+        moveInput = inputValue;
+    }
+}
---
+++ My project/Assets/UI/Joystick/Joystick.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] RectTransform thumbStickTrans;
+    [SerializeField] RectTransform backgroundTrans;
+    [SerializeField] RectTransform centerTrans;
+
+    public delegate void OnStickInputValueUpdated(Vector2 inputVal);
+    public event OnStickInputValueUpdated onStickValueUpdated;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 touchPos = eventData.position;
+        Vector2 centerPos = backgroundTrans.position;
+
+        Vector2 localOffset = Vector2.ClampMagnitude(touchPos - centerPos, backgroundTrans.sizeDelta.x / 2);
+        Vector2 inputVal = localOffset / (backgroundTrans.sizeDelta.x / 2);
+
+        thumbStickTrans.position = centerPos + localOffset;
+        onStickValueUpdated?.Invoke(inputVal);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        backgroundTrans.position = eventData.position;
+        thumbStickTrans.position = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        backgroundTrans.position = centerTrans.position;
+        thumbStickTrans.position = backgroundTrans.position;
+        onStickValueUpdated?.Invoke(Vector2.zero);
+    }
+}
